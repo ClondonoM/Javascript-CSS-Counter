a3f0e3f244f40bb0adc3601e8b234778d0c84252
Carlos Londono <c.londono.m@gmail.com>
2022-04-02 18:46:51 -0500

    js functionalgit add .

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const task = [];
+const tasks = [];
 let time = 0,
   timer = null,
   timerBreak = null,
@@ -7,13 +7,14 @@ let time = 0,
 const bAdd = document.querySelector("#bAdd");
 const itTask = document.querySelector("#itTask");
 const form = document.querySelector("#form");
+const taskName = document.querySelector("#time #taskName");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   if (itTask.value != "") {
     createTask(itTask.value);
     itTask.value = "";
-    renderTask();
+    renderTasks();
   }
 });
 
@@ -23,16 +24,17 @@ function createTask(value) {
     title: value,
     completed: false,
   };
-  task.unshift(newTask);
+  console.log(newTask);
+  tasks.unshift(newTask);
 }
 
-function renderTask() {
-  const html = task.map((task) => {
+function renderTasks() {
+  const html = tasks.map((task) => {
     return `
     <div class="task">
       <div class="completed">${
         task.completed
-          ? `<span class="done">Donde</span>`
+          ? `<span class="done">Done</span>`
           : `<button class="start-button" data-id="${task.id}">Start</button>`
       }</div>
       <div class="title">${task.title}</div>
@@ -40,6 +42,51 @@ function renderTask() {
     </div>`;
   });
 
-  const taskContainer = document.querySelector("#tasks");
-  taskContainer.innerHTML = html.join(" ");
+  const tasksContainer = document.querySelector("#tasks");
+  tasksContainer.innerHTML = html.join("");
+
+  const startButtons = document.querySelectorAll(".task .start-button");
+
+  startButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      if (!timer) {
+        const id = button.getAttribute("data-id");
+        startButtonHandler(id);
+        button.textContent = "In progress...";
+      }
+    });
+  });
+}
+
+function startButtonHandler(id) {
+  time = 25 * 60;
+  current = id;
+  const taskIndex = tasks.findIndex((task) => task.id === id);
+  taskName.textContent = tasks[taskIndex].title;
+
+  timer = setInterval(() => {
+    timerHandler(id);
+  }, 1000);
+}
+
+function timerHandler(id) {
+  time--;
+  renderTime();
+
+  if (time === 0) {
+    clearInterval(timer);
+    current = null;
+    taskName.textContent = "";
+    renderTime();
+  }
+}
+
+function renderTime() {
+  const timeDiv = document.querySelector("#time #value");
+  const minutes = parseInt(time / 60);
+  const seconds = parseInt(time % 60);
+
+  timeDiv.textContent = `${minutes < 10 ? "0" : ""}${minutes}:${
+    seconds < 10 ? "0" : ""
+  }${seconds}`;
 }
